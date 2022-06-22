c2e7aaa018a77daee75a1169c001976b8423c3bc
OmaFineseed <favourfineseed@gmail.com>
2022-06-23 00:06:16 +0100

    initial commit: JavaScript code on screen

---
+++ index.js
@@ -0,0 +1,3 @@
+ document.write ("<h4>NAME:</h4> <h3>My name is Okereke Favour Oluomachi</h3>")
+ document.write ("<h4>HEIGHT:</h4> <h3>I am about 5.0 feet tall</h3>")
+ document.write ("<h4>COUNTRY:</h4> <h3>I am from Nigeria</h3>")
